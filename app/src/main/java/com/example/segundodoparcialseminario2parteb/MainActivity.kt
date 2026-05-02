255c1cb93482b1759f322611ffe5bca8142c1baa
ramiro <ramiro20010202@gmail.com>
2026-05-01 22:49:54 -0600

    initial commit

---
+++ app/src/main/java/com/example/segundodoparcialseminario2parteb/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.segundodoparcialseminario2parteb
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.segundodoparcialseminario2parteb.ui.theme.SegundodoParcialSeminario2ParteBTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SegundodoParcialSeminario2ParteBTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SegundodoParcialSeminario2ParteBTheme {
+        Greeting("Android")
+    }
+}
